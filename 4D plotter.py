56c76dc25a571e29fc01e9c5951b176d165618d3
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-06-16 18:34:52 +0200

    geometry and temperature plotter (4D)

---
+++ 4D plotter.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+from mpl_toolkits.mplot3d import Axes3D
+from tqdm import tqdm
+# Load temperature data
+try:
+    temp = np.load("temperatures_time:15.0000.npy")
+except FileNotFoundError as e:
+    print("check your path, you idiot...")
+
+alpha = 0.3
+mini = 272 # kelvins
+maxi = np.max(temp)
+
+ct = np.empty((len(temp), len(temp[0]), len(temp[0,0]), 4), dtype=np.float16) # voxels and rgba
+for x in tqdm(range(len(temp))):
+    for y in range(len(temp[0])):
+        for z in range(len(temp[0,0])):
+            if temp[x,y,z]>100:
+                ct[x, y, z, 0] = (temp[x, y, z] - mini) / (maxi - mini)  # RGB channels - red-blue lut
+                ct[x,y,z,1]=0.0
+                ct[x,y,z,2]=1.0-(temp[x, y, z] - mini) / (maxi - mini)
+                ct[x, y, z, 3] = alpha  # Alpha channel
+            else:
+                ct[x,y,z,:]=0.0 # if empty, then fill!
+
+fig = plt.figure()
+n = 3 # good for 4 GB of ram!
+ax = fig.add_subplot(111, projection='3d')
+ax.voxels(ct[::n, ::n, ::n, 0], edgecolor='none', facecolors=ct[::n, ::n, ::n, :]) # here 'n' happens
+ax.set(xlabel="X",ylabel="Y",zlabel="Z")
+ax.view_init(elev=25, azim=90) #nice
+
+plt.show()
+# plt.savefig("dummy_first_temperatures.jpg") # use or not to use?
+plt.close(fig)
