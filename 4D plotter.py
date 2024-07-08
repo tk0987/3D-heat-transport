f8e8ef4a3e0933952b3a6fe6cd71faf372c04acb
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-07-08 08:34:20 +0200

    Update 4D plotter.py

--- 4D plotter.py
+++ 4D plotter.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
-# Load temperature data
+# Load temperature data     /home/tomkow1029/Desktop/temp_solver/temp_geo_plotter.py
 try:
-    temp = np.load("temperatures_time:15.0000.npy")
+    temp = np.load("temp_0.npy")
 except FileNotFoundError as e:
-    print("check your path, you idiot...")
+    print("\n\n\n\ncheck your path, you idiot...\n\n\n\n")
 
 alpha = 0.3
-mini = 272 # kelvins
+mini = 292 # kelvins
 maxi = np.max(temp)
 
 ct = np.empty((len(temp), len(temp[0]), len(temp[0,0]), 4), dtype=np.float16) # voxels and rgba
@@ -18,7 +18,7 @@ for x in tqdm(range(len(temp))):
     for y in range(len(temp[0])):
         for z in range(len(temp[0,0])):
             if temp[x,y,z]>100:
-                ct[x, y, z, 0] = (temp[x, y, z] - mini) / (maxi - mini)  # RGB channels - red-blue lut
+                ct[x, y, z, 0] = (temp[x, y, z] - mini)  / (maxi - mini)  # RGB channels - red-blue lut
                 ct[x,y,z,1]=0.0
                 ct[x,y,z,2]=1.0-(temp[x, y, z] - mini) / (maxi - mini)
                 ct[x, y, z, 3] = alpha  # Alpha channel
@@ -32,6 +32,9 @@ ax.voxels(ct[::n, ::n, ::n, 0], edgecolor='none', facecolors=ct[::n, ::n, ::n, :
 ax.set(xlabel="X",ylabel="Y",zlabel="Z")
 ax.view_init(elev=25, azim=90) #nice
 
+plt.show()
+# plt.savefig("dummy_first_temperatures.jpg") # use or not to use?
+plt.close(fig)
 plt.show()
 # plt.savefig("dummy_first_temperatures.jpg") # use or not to use?
 plt.close(fig)
