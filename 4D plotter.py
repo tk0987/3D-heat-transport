7bdc00ef95744f0f3fac8a3625806e9349643bb7
tk0987 <109526789+tk0987@users.noreply.github.com>
2024-07-26 16:58:34 +0200

    colorbar with ticks

--- 4D plotter.py
+++ 4D plotter.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 # Load temperature data     /home/tomkow1029/Desktop/temp_solver/temp_geo_plotter.py
 try:
-    temp = np.load("temperatures_time_0.4000.npy")
+    temp = np.load("temperatures_time_1.8000.npy")
 except FileNotFoundError as e:
     print("\n\n\n\ncheck your path, you idiot...\n\n\n\n")
 
@@ -33,11 +34,22 @@ for x in tqdm(range(len(temp))):
     #     print("fail")
 print("plotting........")
 fig = plt.figure()
-n = 2 # good for 4 GB of ram!
+n = 6 # good for 4 GB of ram!
 ax = fig.add_subplot(111, projection='3d')
 ax.voxels(ct[::n, ::n, ::n, 0], edgecolor='none', facecolors=ct[::n, ::n, ::n, :]) # here 'n' happens
 ax.set(xlabel="X",ylabel="Y",zlabel="Z")
-ax.view_init(elev=40, azim=45) #nice
+ax.view_init(elev=45, azim=45) #nice
+
+norm=Normalize(vmin=mini,vmax=maxi)
+cmap=cm.ScalarMappable(norm=norm,cmap='coolwarm',)
+cmap.set_array([])
+
+num_lev=11
+cbar=fig.colorbar(cmap,ax=ax,shrink=0.5,aspect=10,ticks=np.linspace(mini,maxi,num_lev))
+
+cbar.set_label("T [K]")
+
+
 
 plt.show()
 # plt.savefig("dummy_first_temperatures.jpg") # use or not to use?
